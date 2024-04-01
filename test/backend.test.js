96cc558bc75bf1e4d006d593e4a6381834358d24
Naeem Davids <naeemdav32@gmail.com>
2024-04-01 17:59:23 +0200

    First Commit

---
+++ test/backend.test.js
@@ -0,0 +1,16 @@
+let chai = require('chai');
+let expect = chai.expect;
+let request = require('request');
+
+
+describe('Fetch request to iTunes API should do: ', () => {
+
+   it('Should return all songs', (done) => {
+      this.timeout(5000); // Set timeout to 5 seconds
+      request(`https://itunes.apple.com/search?term=post&entity=musicVideo&limit=1`, function (error, response, body) {
+         expect(response.statusCode).to.equal(200);
+         done();
+      });
+   });
+
+})
